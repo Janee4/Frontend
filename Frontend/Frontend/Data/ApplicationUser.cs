b0df3961f9d724772754facb1abe90fe296d38e8
Jane <hezha.aban@gmail.com>
2024-07-02 16:13:18 +0200

    1:55:31

--- Frontend/Frontend/Data/ApplicationUser.cs
+++ Frontend/Frontend/Data/ApplicationUser.cs
@@ -8,7 +8,7 @@ public class ApplicationUser : IdentityUser
 {
     public string FirstName { get; set; } = null!;
     public string LastName { get; set;} = null!;
-    public string Biography { get; set; }
+    public string? Biography { get; set; }
 
     public string? ProfileImage { get; set; } = "avatar.jpg";
     public int? AddressId { get; set; }
--- Frontend/Frontend/Data/Migrations/20240702140631_AddNewColumnsToApplicationUser.Designer.cs
+++ Frontend/Frontend/Data/Migrations/20240702140631_AddNewColumnsToApplicationUser.Designer.cs
@@ -4,6 +4,7 @@ using Frontend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Frontend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240702140631_AddNewColumnsToApplicationUser")]
+    partial class AddNewColumnsToApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -62,6 +65,7 @@ namespace Frontend.Migrations
                         .HasColumnType("int");
 
                     b.Property<string>("Biography")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("ConcurrencyStamp")
