da95399fee983640ca895c1a7c47b7107c999c6a
Jane <hezha.aban@gmail.com>
2024-07-03 15:52:54 +0200

    2:15:26 lagt in builder.Services.AddHttpClient(); i program.cs

--- Frontend/Frontend/Program.cs
+++ Frontend/Frontend/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+builder.Services.AddHttpClient();
 
 builder.Services.AddAuthentication(options =>
     {
